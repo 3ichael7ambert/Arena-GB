f82e4aeb5494ffaf005f76fd47636ad29175d487
3ichael 7ambert <3ichael7ambert@3ichaels-Laptop.local>
2023-10-24 23:12:39 -0600

    src

---
+++ build/src/src/data/scene_0_projectiles.c
@@ -0,0 +1,46 @@
+#pragma bank 255
+
+// Scene: Arena3
+// Projectiles
+
+#include "gbs_types.h"
+#include "data/sprite_bullet.h"
+
+BANKREF(scene_0_projectiles)
+
+const struct projectile_def_t scene_0_projectiles[] = {
+    {
+        // Projectile 0,
+        .sprite = TO_FAR_PTR_T(sprite_bullet),
+        .move_speed = 32,
+        .life_time = 60,
+        .collision_group = COLLISION_GROUP_1,
+        .collision_mask = COLLISION_GROUP_1,
+        .bounds = {
+            .left = 0,
+            .bottom = -1,
+            .right = 15,
+            .top = -16
+        },
+        .anim_tick = 15,
+        .animations = {
+            {
+                .start = 0,
+                .end = 0
+            },
+            {
+                .start = 1,
+                .end = 1
+            },
+            {
+                .start = 0,
+                .end = 0
+            },
+            {
+                .start = 2,
+                .end = 2
+            }
+        },
+        .initial_offset = 0
+    }
+};
---
+++ build/src/src/data/sprite_hp_tileset.c
@@ -0,0 +1,14 @@
+#pragma bank 255
+
+// Tileset: sprite_hp_tileset
+
+#include "gbs_types.h"
+
+BANKREF(sprite_hp_tileset)
+
+const struct tileset_t sprite_hp_tileset = {
+    .n_tiles = 0,
+    .tiles = {
+        0x00
+    }
+};
