85dc08a3ca7998a4b3ae8b13b1358546c9d00540
3ichael 7ambert <3ichael7ambert@3ichaels-Laptop.local>
2023-10-27 00:04:45 -0600

    edits

---
+++ build/src/src/data/scene_5_actors 2.c
@@ -0,0 +1,88 @@
+#pragma bank 255
+
+// Scene: Arena2
+// Actors
+
+#include "gbs_types.h"
+#include "data/sprite_hp.h"
+#include "data/actor_1_update.h"
+#include "data/sprite_bullet.h"
+#include "data/actor_5_update.h"
+#include "data/sprite_npc003.h"
+#include "data/actor_0_update.h"
+#include "data/actor_0_interact.h"
+
+BANKREF(scene_5_actors)
+
+const struct actor_t scene_5_actors[] = {
+    {
+        // HP,
+        .pos = {
+            .x = 8 * 16,
+            .y = 24 * 16
+        },
+        .bounds = {
+            .left = -8,
+            .bottom = 7,
+            .right = 23,
+            .top = -24
+        },
+        .dir = DIR_DOWN,
+        .sprite = TO_FAR_PTR_T(sprite_hp),
+        .move_speed = 16,
+        .anim_tick = 15,
+        .pinned = TRUE,
+        .persistent = TRUE,
+        .collision_group = COLLISION_GROUP_NONE,
+        .collision_enabled = FALSE,
+        .script_update = TO_FAR_PTR_T(actor_1_update),
+        .reserve_tiles = 0
+    },
+    {
+        // controller,
+        .pos = {
+            .x = 0 * 16,
+            .y = 0 * 16
+        },
+        .bounds = {
+            .left = 0,
+            .bottom = -1,
+            .right = 15,
+            .top = -16
+        },
+        .dir = DIR_DOWN,
+        .sprite = TO_FAR_PTR_T(sprite_bullet),
+        .move_speed = 16,
+        .anim_tick = 15,
+        .pinned = FALSE,
+        .persistent = TRUE,
+        .collision_group = COLLISION_GROUP_NONE,
+        .collision_enabled = TRUE,
+        .script_update = TO_FAR_PTR_T(actor_5_update),
+        .reserve_tiles = 0
+    },
+    {
+        // Monkey,
+        .pos = {
+            .x = 144 * 16,
+            .y = 56 * 16
+        },
+        .bounds = {
+            .left = 0,
+            .bottom = 7,
+            .right = 15,
+            .top = -8
+        },
+        .dir = DIR_DOWN,
+        .sprite = TO_FAR_PTR_T(sprite_npc003),
+        .move_speed = 16,
+        .anim_tick = 15,
+        .pinned = FALSE,
+        .persistent = TRUE,
+        .collision_group = COLLISION_GROUP_2,
+        .collision_enabled = TRUE,
+        .script_update = TO_FAR_PTR_T(actor_0_update),
+        .script = TO_FAR_PTR_T(actor_0_interact),
+        .reserve_tiles = 0
+    }
+};
